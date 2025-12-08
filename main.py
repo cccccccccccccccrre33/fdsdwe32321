a93481b465ac0779e56528fcd23916cdbba104b8
cccccccccccccccrre33 <eresenkovalentin@gmail.com>
2025-12-08 13:07:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from dotenv import load_dotenv
+load_dotenv()
 import os
-import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Получаем токен из переменной окружения
+# Берём токен из переменной окружения
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 # Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот запущен и готов работать!")
+    await update.message.reply_text("Бот запущен и работает!")
 
+# Основная функция
 async def main():
     # Создаём приложение
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
@@ -18,8 +20,9 @@ async def main():
     app.add_handler(CommandHandler("start", start))
 
     # Запускаем бота
-    print("Бот стартовал...")
     await app.run_polling()
 
+# Запуск
 if __name__ == "__main__":
+    import asyncio
     asyncio.run(main())
